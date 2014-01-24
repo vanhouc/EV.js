e4b523b769e1a838c1d06cc87dcfa182cd6de4a2
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-24 09:44:40 -0500

    Added svg interaction

--- js/evScript.js
+++ js/evScript.js
@@ -1,12 +1,31 @@
 ﻿var plan,
     svgReq,
     i,
+    mousePosition,
     init = function (data) {
         plan = project.importSVG(data);
     };
 $(document).ready(function () {
-    svgReq = $.get("svg/Y11_Infusion.svg");
+    svgReq = $.get("svg/Y11_Infusion2.svg");
     svgReq.done(function (data) {
         init(data);
     });
 });
+function onKeyDown(event) {
+    if (event.key == 'up') {
+        plan.scale(1.1, mousePosition);
+        return false;
+    }
+    if (event.key == 'down') {
+        plan.scale(0.9, mousePosition);
+        return false;
+    }
+    alert(event.key);
+}
+tool.minDistance = 10;
+function onMouseDrag(event) {
+    view.scrollBy(event.delta);
+}
+function onMouseMove(event) {
+    mousePosition = event.point;
+}
